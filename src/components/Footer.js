208d2cc11801adf13f584328f422b901af444683
ARCHI-Ufanet <archi481516@gmail.com>
2023-03-17 01:17:16 +0300

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-    return (
-        <footer className="footer">
-            <p className="footer__text">&copy; {(new Date()).getFullYear()} Mesto Russia</p>
-        </footer>
-    );
-};
